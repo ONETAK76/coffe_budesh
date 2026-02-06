90de15e7575ae3a8142cf7f5bcba7e19d1f9c6a0
Иван <158520912+ONETAK76@users.noreply.github.com>
2026-02-06 12:52:47 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+API_TOKEN = '8592491073:AAHMGeIOkG5DZlaW8UHpE9TJ3oM7Uk-4jWg'
+ADMIN_IDS = [1368428868, 316438597]
+BOT_USERNAME = 'coffee_budes_bot'
+REFERRAL_REWARD = 50.0
+
+# URL вашего WebApp (после деплоя на Render замените на актуальный)
+WEB_APP_URL = "https://coffee-budes.onrender.com" 
